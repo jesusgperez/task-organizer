9a9b9226211bb7ccde431ca4f361585a472aa15c
Jesus Perez <jesus.perez@uplight.com>
2023-12-18 17:18:37 -0500

    Something related to the UI

--- app/models/task.rb
+++ app/models/task.rb
@@ -22,6 +22,8 @@ class Task < ApplicationRecord
   # Custom validations
   validate :due_date_validity
 
+  accepts_nested_attributes_for :participating_users, allow_destroy: true
+
   def due_date_validity
     return if due_date.blank?
     return if due_date > Date.today
